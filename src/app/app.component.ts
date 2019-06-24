1088748761be1dad589fa74ecc4734e6473b9ea6
Richard Hensler <richard.hensler@gmail.com>
2019-06-24 00:34:05 -0700

    restructure app to use streams

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,38 @@
-import { Component, OnInit } from '@angular/core';
-import { CardService } from './services/card.service';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { combineLatest, Observable, Subject, Subscription } from 'rxjs';
+import { mergeMap, tap } from 'rxjs/operators';
 import { ZingoCard } from './models/zingo-card';
+import { CardService } from './services/card.service';
 
 @Component({
     selector: 'app-root',
     templateUrl: './app.component.html',
     styleUrls: ['./app.component.css']
 })
-export class AppComponent implements OnInit {
+export class AppComponent implements OnInit, OnDestroy {
     public card: ZingoCard;
     public confirmed = false;
     public title = 'Zingo';
     private cardCount: number;
     private cardNumber: number;
+    private card$: Subject<number>;
+    private confirm$: Subject<boolean>;
+    private getResult: Subscription;
 
     constructor(private cardService: CardService) {}
 
     ngOnInit() {
-        this.cardService.cardCount().subscribe(count => this.cardCount = count);
-        this.cardNumber = 1;
-        this.getCard(this.cardNumber);
+        this.setupStreams();
+        this.setupCards();
     }
 
-    public getCard(newCardNumber: number) {
-        this.cardService.getCard(newCardNumber).subscribe(
-            result => this.card = result,
-            err => console.warn(err)
-        );
+    ngOnDestroy() {
+        this.getResult.unsubscribe();
+        this.card$.unsubscribe();
     }
 
     public confirmCard(confirmed: boolean) {
+        this.confirm$.next(confirmed);
         this.confirmed = !!confirmed;
     }
 
@@ -37,6 +40,30 @@ export class AppComponent implements OnInit {
         // only get available cardNumbers (1 to number of cards)
         const tempCardNumber = this.cardNumber + increment;
         this.cardNumber = tempCardNumber > 0 && tempCardNumber <= this.cardCount ? tempCardNumber : tempCardNumber % this.cardCount;
-        this.getCard(this.cardNumber);
+        this.card$.next(this.cardNumber);
+    }
+
+    private getCard(subj: Subject<number>): Observable<ZingoCard> {
+        return subj.pipe(
+            mergeMap(cardNo => this.cardService.getCard(cardNo)),
+            tap(result => this.card = result)
+        );
+    }
+
+    private setupCards() {
+        this.cardService.cardCount().subscribe(count => this.cardCount = count);
+        this.cardNumber = 1;
+        this.card$.next(this.cardNumber);
+    }
+
+    private setupStreams() {
+        this.card$ = new Subject();
+        this.confirm$ = new Subject();
+        const cardSelect$ = this.getCard(this.card$);
+        combineLatest(cardSelect$, this.confirm$)
+            .pipe(
+                tap(() => console.log('received and clicked'))
+            )
+            .subscribe(() => {}, err => console.warn(err));
     }
 }
