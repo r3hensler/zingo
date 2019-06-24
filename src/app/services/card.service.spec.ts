12e09387203f69c90376abad45b50d4e19851d56
Richard Hensler <richard.hensler@gmail.com>
2019-06-23 23:21:15 -0700

    test cardService methods

--- src/app/services/card.service.spec.ts
+++ src/app/services/card.service.spec.ts
@@ -1,16 +1,55 @@
-import { TestBed } from '@angular/core/testing';
-import { CardService } from './card.service';
 import { HttpClient } from '@angular/common/http';
-
-const mockHttpClient = jasmine.createSpyObj('httpService', ['get']);
+import { TestBed } from '@angular/core/testing';
+import { of } from 'rxjs';
+import { CardService } from './card.service';
+import { ZingoCard } from '../models/zingo-card';
 
 describe('CardService', () => {
-    beforeEach(() => TestBed.configureTestingModule({
-        providers: [{provide: HttpClient, useValue: mockHttpClient}]
-    }));
+    const expectedCardGet = new ZingoCard();
+    const mockHttpClient = jasmine.createSpyObj('httpService', ['get']);
+    const urlRoot = 'http://localhost:3000';
 
-    it('should be created', () => {
-        const service: CardService = TestBed.get(CardService);
-        expect(service).toBeTruthy();
+    beforeEach(() => {
+        mockHttpClient.get.calls.reset();
+        mockHttpClient.get.and.callFake(url => {
+            console.log(url);
+            if (url === `${urlRoot}/cards/1`) {
+                return of(expectedCardGet);
+            } else if (url === `${urlRoot}/cards`) {
+                return of([expectedCardGet, expectedCardGet]);
+            } else {
+                return of(null);
+            }
+        });
+
+        TestBed.configureTestingModule({
+            providers: [{provide: HttpClient, useValue: mockHttpClient}]
+        });
+    });
+
+    describe('getCard', () => {
+        it('on success', done => {
+            const service: CardService = TestBed.get(CardService);
+            service.getCard().subscribe(
+                result => {
+                    expect(result).toEqual(expectedCardGet);
+                    done();
+                },
+                fail
+            );
+        });
+    });
+
+    describe('cardCount', () => {
+        it('on success', done => {
+            const service: CardService = TestBed.get(CardService);
+            service.cardCount().subscribe(
+                result => {
+                    expect(result).toEqual(2);
+                    done();
+                },
+                fail
+            );
+        });
     });
 });
